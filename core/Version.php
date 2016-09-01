matomo-org/matomo

adds update script to change existing scheduled reports to use utc time

core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.16.2';
+    const VERSION = '2.16.3-b1';
 
     public function isStableVersion($version)
     {

nahueldotme/piwik

core/Version.php
@@ -20,7 +20,7 @@ final class Version
      * The current Piwik version.
      * @var string
      */
-    const VERSION = '2.16.3-b2';
+    const VERSION = '2.16.3-b3';
 
     public function isStableVersion($version)
     {